90aa5f02a9af815fc39dc80afce541d9bb72286e
Christian Lewis <christianlewis024@gmail.com>
2020-05-07 10:13:37 -0700

    dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const postsRouter = require("./router");
 const server = express();
+
+const PORT = process.env.PORT || 6001;
 server.get("/", (req, res) => {
   res.json({ api: "Project API is up and Running!" });
 });
 server.use(express.json());
 server.use("/api/posts", postsRouter);
 
-server.listen(4001, () => console.log("server is up and running! port 4001"));
+server.listen(PORT, () => {
+  console.log(`server runnong on ${PORT}`);
+});
